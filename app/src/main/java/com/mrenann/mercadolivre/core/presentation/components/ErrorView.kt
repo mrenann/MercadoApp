40cfc70cc12e3cb9dd9bbff1d4096617cb4d7757
mrenann <marcos.renann.br@gmail.com>
2025-05-11 12:46:41 -0300

    feat: search products

--- app/src/main/java/com/mrenann/mercadolivre/core/presentation/components/ErrorView.kt
+++ app/src/main/java/com/mrenann/mercadolivre/core/presentation/components/ErrorView.kt
@@ -1,6 +1,5 @@
 package com.mrenann.mercadolivre.core.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
