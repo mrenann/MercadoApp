4573059d35f2da03b949aa7fd598f58df09b3ec8
mrenann <marcos.renann.br@gmail.com>
2025-05-10 23:50:54 -0300

    feat: di

--- app/src/main/java/com/mrenann/mercadolivre/MainApplication.kt
+++ app/src/main/java/com/mrenann/mercadolivre/MainApplication.kt
@@ -3,6 +3,7 @@ package com.mrenann.mercadolivre
 import android.app.Application
 import android.util.Log
 import com.mrenann.mercadolivre.core.di.networkModule
+import com.mrenann.mercadolivre.searchScreen.di.searchModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 import org.koin.core.error.KoinApplicationAlreadyStartedException
@@ -15,6 +16,7 @@ class MainApplication : Application() {
                 androidContext(applicationContext)
                 modules(
                     networkModule,
+                    searchModule,
                 )
             } catch (e: KoinApplicationAlreadyStartedException) {
                 Log.e("Koin", "Koin already started", e)
---
+++ app/src/main/java/com/mrenann/mercadolivre/searchScreen/di/searchModule.kt
@@ -0,0 +1,22 @@
+package com.mrenann.mercadolivre.searchScreen.di
+
+import com.mrenann.mercadolivre.core.data.remote.MockApiService
+import com.mrenann.mercadolivre.searchScreen.data.repository.SearchRepositoryImpl
+import com.mrenann.mercadolivre.searchScreen.data.source.SearchDataSourceImpl
+import com.mrenann.mercadolivre.searchScreen.domain.repository.SearchRepository
+import com.mrenann.mercadolivre.searchScreen.domain.source.SearchDataSource
+import org.koin.dsl.module
+
+val searchModule =
+    module {
+        single<SearchDataSource> {
+            SearchDataSourceImpl(
+                service = get<MockApiService>(),
+            )
+        }
+        single<SearchRepository> {
+            SearchRepositoryImpl(
+                dataSource = get<SearchDataSource>(),
+            )
+        }
+    }
