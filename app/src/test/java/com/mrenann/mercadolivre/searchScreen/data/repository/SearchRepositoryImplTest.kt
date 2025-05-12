544caa8825d4670eb9f3d7697eb2001adf9f35b9
mrenann <marcos.renann.br@gmail.com>
2025-05-12 20:38:50 -0300

    test: add unit tests for SearchRepositoryImpl

--- app/src/test/java/com/mrenann/mercadolivre/searchScreen/data/repository/SearchRepositoryImplTest.kt
+++ app/src/test/java/com/mrenann/mercadolivre/searchScreen/data/repository/SearchRepositoryImplTest.kt
@@ -1,6 +1,9 @@
 package com.mrenann.mercadolivre.searchScreen.data.repository
 
 import app.cash.turbine.test
+import com.mrenann.mercadolivre.core.data.remote.mappers.toDomain
+import com.mrenann.mercadolivre.core.data.remote.model.search.Result
+import com.mrenann.mercadolivre.core.data.remote.model.search.Shipping
 import com.mrenann.mercadolivre.core.data.remote.response.SearchQueryResponse
 import com.mrenann.mercadolivre.core.utils.Resource
 import com.mrenann.mercadolivre.searchScreen.domain.source.SearchDataSource
@@ -17,16 +20,63 @@ import org.junit.Test
 class SearchRepositoryImplTest {
 
     @Test
-    fun `should repository delegate to dataSource`() = runTest {
-        val fakeResponse = SearchQueryResponse()
+    fun `when search is called, repository should return mapped domain model from dataSource`() =
+        runTest {
+            val fakeResponse = SearchQueryResponse(
+                results = listOf(
+                    Result(
+                        id = "1",
+                        title = "arroz",
+                        thumbnail = "",
+                        originalPrice = 20.0,
+                        shipping = Shipping(
+                            freeShipping = true
+                        ),
+                        currencyId = "BRL",
+                        price = 10.0,
+                    )
+                )
+            )
+
+
+            val dataSource = mockk<SearchDataSource>()
+            coEvery { dataSource.search("arroz") } returns flowOf(Resource.Success(fakeResponse))
+
+            val repository = SearchRepositoryImpl(dataSource)
+
+            val expected = Resource.Success(fakeResponse.results?.map { it.toDomain() })
+            repository.searchProducts("arroz").test {
+                assertEquals(expected, awaitItem())
+                awaitComplete()
+            }
+        }
+
+    @Test
+    fun `when dataSource emits Loading, repository should emit Loading`() = runTest {
         val dataSource = mockk<SearchDataSource>()
-        coEvery { dataSource.search("arroz") } returns flowOf(Resource.Success(fakeResponse))
+        coEvery { dataSource.search("arroz") } returns flowOf(Resource.Loading)
 
         val repository = SearchRepositoryImpl(dataSource)
 
         repository.searchProducts("arroz").test {
-            assertEquals(Resource.Success(fakeResponse), awaitItem())
+            assertEquals(Resource.Loading, awaitItem())
             awaitComplete()
         }
     }
+
+    @Test
+    fun `when dataSource emits Error, repository should emit Error with same message`() = runTest {
+        val errorMessage = "Algo deu errado"
+        val dataSource = mockk<SearchDataSource>()
+        coEvery { dataSource.search("arroz") } returns flowOf(Resource.Error(errorMessage))
+
+        val repository = SearchRepositoryImpl(dataSource)
+
+        repository.searchProducts("arroz").test {
+            assertEquals(Resource.Error(errorMessage), awaitItem())
+            awaitComplete()
+        }
+    }
+
+
 }
