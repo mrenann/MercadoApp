a80f6376afcb7d934cfc2bdd41ae7b3eeb28e605
mrenann <marcos.renann.br@gmail.com>
2025-05-11 23:47:15 -0300

    feat: integrate item details

--- app/src/main/java/com/mrenann/mercadolivre/MainApplication.kt
+++ app/src/main/java/com/mrenann/mercadolivre/MainApplication.kt
@@ -3,6 +3,7 @@ package com.mrenann.mercadolivre
 import android.app.Application
 import android.util.Log
 import com.mrenann.mercadolivre.core.di.networkModule
+import com.mrenann.mercadolivre.detailsScreen.di.detailsModule
 import com.mrenann.mercadolivre.searchScreen.di.searchModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
@@ -17,6 +18,7 @@ class MainApplication : Application() {
                 modules(
                     networkModule,
                     searchModule,
+                    detailsModule
                 )
             } catch (e: KoinApplicationAlreadyStartedException) {
                 Log.e("Koin", "Koin already started", e)
