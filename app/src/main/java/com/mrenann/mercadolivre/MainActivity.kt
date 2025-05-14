bec37bd908d4984178f8590015b2bc5c5c704fc7
mrenann <marcos.renann.br@gmail.com>
2025-05-14 20:03:02 -0300

    feat: Integrate Lyricist for string localization

--- app/src/main/java/com/mrenann/mercadolivre/MainActivity.kt
+++ app/src/main/java/com/mrenann/mercadolivre/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import cafe.adriel.lyricist.ProvideStrings
+import cafe.adriel.lyricist.rememberStrings
 import cafe.adriel.voyager.navigator.Navigator
 import com.mrenann.mercadolivre.homeScreen.presentation.screens.HomeScreen
 import com.mrenann.mercadolivre.ui.theme.MercadoLivreTheme
@@ -18,20 +20,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MercadoLivreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier =
-                            Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding),
-                    ) {
-                        Navigator(
-                            HomeScreen(),
-                        )
+            val lyricist = rememberStrings()
+            ProvideStrings(lyricist = lyricist) {
+                MercadoLivreTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Column(
+                            modifier =
+                                Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding),
+                        ) {
+                            Navigator(
+                                HomeScreen(),
+                            )
+                        }
                     }
                 }
             }
         }
+
     }
 }
