7e26ed73478032076a363854fcdcfcd1cb4303fb
mrenann <marcos.renann.br@gmail.com>
2025-05-14 23:14:54 -0300

    refactor: make SearchResult serializable

--- app/src/main/java/com/mrenann/mercadolivre/core/domain/model/SearchResult.kt
+++ app/src/main/java/com/mrenann/mercadolivre/core/domain/model/SearchResult.kt
@@ -1,11 +1,13 @@
 package com.mrenann.mercadolivre.core.domain.model
 
+import java.io.Serializable
+
 data class SearchResult(
     val thumbnail: String? = "",
     val title: String? = "",
     val originalPrice: Double? = 0.0,
     val price: Double? = 0.0,
     val freeShipping: Boolean? = false,
-    val currencyId: String?,
-    val id: String?,
-)
+    val currencyId: String? = null,
+    val id: String? = null
+) : Serializable
